690288d5341d304a8540ae823ed03f31671d6cac
worachot-n <worachotn@gmail.com>
2026-03-30 11:46:11 +0700

    fix bug

--- config.py
+++ config.py
@@ -7,4 +7,7 @@ class Config:
         'postgresql://user:password@localhost:5432/supply_db'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'connect_args': {'client_encoding': 'utf8'}
+    }
     DEBUG = os.environ.get('DEBUG', 'true').lower() == 'true'
